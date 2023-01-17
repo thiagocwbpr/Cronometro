0e46abc20576c1e9ec29e90a33fbeed10929407d
Thiago Menezes <thiagocwbpr@gmail.com>
2023-01-17 16:04:39 -0300

    Criado a função que será responsável pelo cronometro. utilizei uma função WHILE, pois no meu ver, pode ser mais eficiente. Também utilizei um 'sleep' Thread para que de um intervalo de 1 segundo a cada ciclo de iteração no WHILE, e também que a cada ciclo seja realizado um 'clear' na tela, para que os numeros fiquem bem evidente na tela. E também criei uma condição IF, para que quando o cronometro chegar a 0, possa exibir uma mensagem ao usuário, ou ativar um alarme (a criatividade é livre).

--- Cronometro/Program.cs
+++ Cronometro/Program.cs
@@ -1,2 +1,23 @@
 ﻿
 
+using System.ComponentModel.Design;
+using System.Reflection;
+
+
+static void inicio(int time) {
+
+	while (time != 0) {
+
+        Console.Clear();
+        time = time - 1;
+        Console.WriteLine($"\n\n\t{time}\n\n");
+        Thread.Sleep(1000);
+        if (time == 0) {
+            Console.Clear();
+            Console.WriteLine("\n\n\tBAIXE A CABEÇA E VAI ESTUDAR!!!\n\n\n");
+            Console.WriteLine("\n\nO CRONOMETRO ZEROU! CLIQUE EM QUALQUER TECLA PARA VOLTAR AO MENU PRINCIPAL.");
+            Console.ReadKey();
+        }
+        
+    }
+}

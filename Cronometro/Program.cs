c9009907ec3145997f58f10b3a873a3379c93877
Thiago Menezes <thiagocwbpr@gmail.com>
2023-01-17 16:38:49 -0300

    Modificado o Menu Principal. Foi adicionado a opção de ordem crescente e decrescente do cronometro. Portanto o usuário pode escolher essa interação do cronometro. Também foi criado uma nova condição dentro da função do cronometro, para que seja possível usar o modo crescente e decrescente do cronometro.

--- Cronometro/Program.cs
+++ Cronometro/Program.cs
@@ -11,23 +11,51 @@ static void Menu() {
     Console.WriteLine("\nCRONOMETRO DIGITAL!\n");
     Console.Write("\n\nInforme o tempo que deseja cronometrar: ");
     int timeMenu = int.Parse(Console.ReadLine());
+    Console.WriteLine("\nVocê deseja cronometrar de qual forma ?");
+    Console.WriteLine("1 - Crescente");
+    Console.WriteLine("2 - decrescente");
+    Console.Write("\nEscolha uma das opções numeros acima: ");
+    int tipoCrono = int.Parse(Console.ReadLine());
+    
 
-    inicio(timeMenu);
+    inicio(timeMenu, tipoCrono);
 }
-static void inicio(int time) {
+static void inicio(int time, int tipoCronmetro) {
 
-	while (time != 0) {
+    if (tipoCronmetro == 1) {
 
-        Console.Clear();
-        time--;
-        Console.WriteLine($"\n\n\t{time}\n\n");
-        Thread.Sleep(1000);
-        if (time == 0) {
+        int time2 = 0;
+
+        while (time2 <= time) {
+
             Console.Clear();
-            Console.WriteLine("\n\n\tBAIXE A CABEÇA E VAI ESTUDAR!!!\n\n\n");
-            Console.WriteLine("\n\nO CRONOMETRO ZEROU! CLIQUE EM QUALQUER TECLA PARA VOLTAR AO MENU PRINCIPAL.");
-            Console.ReadKey();
-            Menu();
+            time2++;
+            Console.WriteLine($"\n\n\t{time2}\n\n");
+            Thread.Sleep(1000);
+            if (time2 == 20) {
+                Console.Clear();
+                Console.WriteLine("\n\n\tTIME IS UP!!!\n\n\n");
+                Console.WriteLine("\n\nO CRONOMETRO ZEROU! CLIQUE EM QUALQUER TECLA PARA VOLTAR AO MENU PRINCIPAL.");
+                Console.ReadKey();
+                Menu();
+            }
+        }
+
+    }
+    else {
+        while (time != 0) {
+
+            Console.Clear();
+            time--;
+            Console.WriteLine($"\n\n\t{time}\n\n");
+            Thread.Sleep(1000);
+            if (time == 0) {
+                Console.Clear();
+                Console.WriteLine("\n\n\tTIME IS UP!!!\n\n\n");
+                Console.WriteLine("\n\nO CRONOMETRO ZEROU! CLIQUE EM QUALQUER TECLA PARA VOLTAR AO MENU PRINCIPAL.");
+                Console.ReadKey();
+                Menu();
+            }
         }
     }
 }

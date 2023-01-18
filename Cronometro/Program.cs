c94665472b7e68c46500378b5b24f8ec10569419
Thiago Menezes <thiagocwbpr@gmail.com>
2023-01-18 14:03:33 -0300

    Finalizado o Cronometro! Realizado alguns ajustes de menu e ajustes na função de calculo do cronometro. Também ajustes realizados na opção de sair da aplicação de forma correta.

--- Cronometro/Program.cs
+++ Cronometro/Program.cs
@@ -11,32 +11,42 @@ static void Menu() {
     Console.WriteLine("\nCRONOMETRO DIGITAL!\n");
     Console.Write("\nS = Segundo => 10s = 10 segundos ");
     Console.Write("\nM = Minuto => 1m = 1 minuto");
-    Console.Write("\n0 = Sair\n");
+    Console.Write("\n00 = Sair\n");
     Console.Write("\nQuanto tempo deseja contar?  ");
     string data = Console.ReadLine().ToLower();
-    Console.WriteLine("\nDeseja usar o cronometro de forma Crescente ou Decrescente? ");
-    Console.WriteLine("1 - Crescente");
-    Console.WriteLine("2 - Decrescente");
-    Console.Write("\nDigite a opção numerica informada acima : ");
-    int typeCronometro = int.Parse(Console.ReadLine());
-
 
     int time = int.Parse(data.Substring(0, data.Length - 1));
     char type = char.Parse(data.Substring(data.Length - 1, 1));
 
     int multiplier = 1;
+    int typeCronometro = 0;
 
-    if (type == 'm')
+    
+    if (type == 'm') {
         multiplier = 60;
-    if (time == 0)
+        Console.WriteLine("\nDeseja usar o cronometro de forma Crescente ou Decrescente? ");
+        Console.WriteLine("1 - Crescente");
+        Console.WriteLine("2 - Decrescente");
+        Console.Write("\nDigite a opção numerica informada acima : ");
+        typeCronometro = int.Parse(Console.ReadLine());
+    }
+    if (time == 0) {
         System.Environment.Exit(0);
+    }
 
     inicio(multiplier * time, typeCronometro);
-}
+    }
 static void inicio(int time, int typeCrono) {
 
     if (typeCrono == 1) {
         int typeCrono2 = 0;
+        Console.Clear();
+        Console.WriteLine("\nReady...\n");
+        Thread.Sleep(1000);
+        Console.WriteLine("\nSet...\n");
+        Thread.Sleep(1000);
+        Console.WriteLine("\nGo...\n");
+        Thread.Sleep(2500);
         while (time != typeCrono2) {
             Console.Clear();
             typeCrono2++;
@@ -48,6 +58,13 @@ static void inicio(int time, int typeCrono) {
         Menu();
     }
     else {
+        Console.Clear();
+        Console.WriteLine("Ready...");
+        Thread.Sleep(1000);
+        Console.WriteLine("Set...");
+        Thread.Sleep(1000);
+        Console.WriteLine("Go...");
+        Thread.Sleep(2500);
         while (time != 0) {
             Console.Clear();
             time--;
